9b9a8830343e896c7d0600ebd78dd25f32d59c12
Eseza Muwanga Najjombwe <43315519+titan256@users.noreply.github.com>
2023-07-22 18:24:25 +0300

    Update settings.py

--- _keenthemes/settings.py
+++ _keenthemes/settings.py
@@ -29,7 +29,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-blehs+vjho@s4qug%9ferf0-tucvbr9#_1k2!#ebs4u68@)ss+"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
